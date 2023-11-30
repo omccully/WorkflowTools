4ac5fff44f4898728530a47e4c1734be9df25b87
omccully <oscarmccully@gmail.com>
2023-11-30 11:31:19 -0600

    create gcor

---
+++ Executable/gcor.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+
+if "%1"=="" (
+   set i=0
+    for /f "tokens=* delims=" %%a in ('git branch --format "%%(refname:short)"') do (
+        set /a i+=1
+        set branch[!i!]=%%a
+    )
+) else (
+    set i=0
+    for /f "tokens=* delims=" %%a in ('git branch --format "%%(refname:short)" ^| find /i "%1"') do (
+        set /a i+=1
+        set branch[!i!]=%%a
+    )
+)
+
+echo Select a branch:
+for /l %%i in (1,1,%i%) do (
+    echo [%%i] !branch[%%i]!
+)
+
+set /p selection="Enter branch number: "
+
+git checkout !branch[%selection%]!
