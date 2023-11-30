07799c9b8068ed6fd32eee8e8ee434666df8b7a5
omccully <oscarmccully@gmail.com>
2023-11-30 10:50:58 -0600

    fully implement gplo

--- Executable/gplo.bat
+++ Executable/gplo.bat
@@ -1,2 +1,11 @@
 @echo off
-git pull origin %1
+
+if "%1"=="" (
+    for /f "tokens=1,2,3" %%i in ('git remote show origin') do (
+        if "%%i"=="HEAD" (
+            git pull origin %%k
+        )
+    )
+) else (
+    git pull origin %1
+)
