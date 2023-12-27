9ce65b3a1330cc8508966d569b74c560592bd3fc
omccully <oscarmccully@gmail.com>
2023-12-27 14:51:37 -0600

    change github batch file to be a python script for maintainability

---
+++ Executable/Python/github.py
@@ -0,0 +1,16 @@
+import re
+import webbrowser
+import sys
+import subprocess
+
+url = subprocess.check_output(["git", "remote", "get-url", "origin"]).decode("utf-8").strip()
+
+url = re.sub(r'^ssh:\/\/git@(sshhub|github)\.com(:[\d]+)\/', 'https://github.com/', url)
+
+if url.endswith(".git"):
+    url = url[:-4]
+
+if len(sys.argv) == 2 and sys.argv[1] == "actions":
+    url += "/actions"
+
+webbrowser.open(url)
--- Executable/github.bat
+++ Executable/github.bat
@@ -1,17 +1,2 @@
 @echo off
-setlocal EnableDelayedExpansion
-
-for /f "tokens=*" %%a in ('git remote get-url origin') do (
-    set "url=%%a"
-)
-
-set "url=!url:.git=!"
-
-set arg=%1
-
-if not "%arg%"=="" (
-    set url=%url%/%arg%
-)
-
-echo Launching %url%
-start "" "!url!"
+py %~dp0\Python\github.py %*
